31afb4ca758ee73310f6f8bf255c68fb8bf273be
Dylan DesRosier <dylan@pooltogether.com>
2021-04-27 11:40:40 -0400

    fix dist

--- packages/api-runner/dist/lib/services/calculateEstimatedPoolPrize.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var ethers = require('ethers');
-require('@pooltogether/current-pool-data');
-
-ethers.ethers.BigNumber.from; // totalSupply     = ticketSupply + sponsorshipSupply
--- packages/api-runner/dist/lib/utils/secondsSinceEpoch.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-const secondsSinceEpoch = () => Number((Date.now() / 1000).toFixed(0));
-
-exports.secondsSinceEpoch = secondsSinceEpoch;
