858f824366c93b05314022ed352910851887828b
Charles Bergeron <chuck.bergeron@gmail.com>
2021-08-25 16:33:43 -0700

    * celo is live on api

--- packages/api-runner/lib/utils/getDefaultPoolAddresses.js
+++ packages/api-runner/lib/utils/getDefaultPoolAddresses.js
@@ -2,5 +2,7 @@ import { prizePoolContracts } from '@pooltogether/current-pool-data'
 
 export const getDefaultPoolAddresses = (chainId) => {
   console.log({chainId})
+  console.log(prizePoolContracts)
+  console.log(prizePoolContracts[chainId])
   return prizePoolContracts[chainId].governance.map((pool) => pool.prizePool.address)
 }
