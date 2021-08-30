b21e6fd8d1aee9e602be53f4e715c5ace0317bc9
Charles Bergeron <chuck.bergeron@gmail.com>
2021-08-30 13:05:44 -0700

    * update api with new polygon token faucets

--- packages/api-runner/lib/utils/getDefaultPoolAddresses.js
+++ packages/api-runner/lib/utils/getDefaultPoolAddresses.js
@@ -1,8 +1,5 @@
 import { prizePoolContracts } from '@pooltogether/current-pool-data'
 
 export const getDefaultPoolAddresses = (chainId) => {
-  console.log({chainId})
-  console.log(prizePoolContracts)
-  console.log(prizePoolContracts[chainId])
   return prizePoolContracts[chainId].governance.map((pool) => pool.prizePool.address)
 }
