4342e761a3712a424cb820eb332ad6441c793c96
Dylan DesRosier <dylan@pooltogether.com>
2021-09-03 10:12:23 -0400

    wip

--- packages/api-tsunami-prizes/getUsersPrizes.js
+++ packages/api-tsunami-prizes/getUsersPrizes.js
@@ -1,156 +1,156 @@
-import { log } from '../../utils/sentry'
-import { contract } from '@pooltogether/etherplex'
+// import { log } from '../../utils/sentry'
+// import { contract } from '@pooltogether/etherplex'
 
-import { batch } from './cloudflare-workers-batch'
-import { ethers } from 'ethers'
-import { isValidAddress, NETWORK } from '@pooltogether/utilities'
+// import { batch } from './cloudflare-workers-batch'
+// import { ethers } from 'ethers'
+// import { isValidAddress, NETWORK } from '@pooltogether/utilities'
 
-const SUPPORTED_NETWORKS = [NETWORK.mainnet, NETWORK.rinkeby, NETWORK.polygon]
-const MOCK_POOLS = Object.freeze({
-  [NETWORK.mainnet]: {
-    config: { chainId: NETWORK.mainnet },
-    prizePool: { address: '0xde9ec95d7708B8319CCca4b8BC92c0a3B70bf416' },
-    prizeStrategy: { address: '0x3D9946190907aDa8b70381b25c71eB9adf5f9B7b' },
-    tokens: {
-      ticket: {
-        decimals: 6,
-        address: '0xD81b1A8B1AD00Baa2D6609E0BAE28A38713872f7'
-      },
-      underlyingToken: {
-        decimals: 6,
-        address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
-      }
-    }
-  },
-  [NETWORK.rinkeby]: {
-    config: { chainId: NETWORK.rinkeby },
-    prizePool: { address: '0x4706856fa8bb747d50b4ef8547fe51ab5edc4ac2' },
-    prizeStrategy: { address: '0x5E0A6d336667EACE5D1b33279B50055604c3E329' },
-    tokens: {
-      ticket: {
-        decimals: 18,
-        address: '0x4fb19557fbd8d73ac884efbe291626fd5641c778'
-      },
-      underlyingToken: {
-        decimals: 18,
-        address: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea'
-      }
-    }
-  }
-})
-const MOCK_PRIZES = {
-  0: [
-    {
-      amountUnformatted: ethers.BigNumber.from('5000000000000000000'),
-      distributionIndex: 2,
-      drawId: 0,
-      pickIndex: 4
-    }
-  ],
-  1: [
-    {
-      amountUnformatted: ethers.BigNumber.from('5000000000000000000'),
-      distributionIndex: 2,
-      drawId: 1,
-      pickIndex: 4
-    }
-  ],
-  2: [
-    {
-      amountUnformatted: ethers.BigNumber.from('5000000000000000000'),
-      distributionIndex: 2,
-      drawId: 2,
-      pickIndex: 13
-    },
-    {
-      amountUnformatted: ethers.BigNumber.from('10000000000000000000'),
-      distributionIndex: 1,
-      drawId: 2,
-      pickIndex: 1
-    }
-  ],
-  3: [
-    {
-      amountUnformatted: ethers.BigNumber.from('10000000000000000000000'),
-      distributionIndex: 0,
-      drawId: 3,
-      pickIndex: 1
-    }
-  ]
-}
+// const SUPPORTED_NETWORKS = [NETWORK.mainnet, NETWORK.rinkeby, NETWORK.polygon]
+// const MOCK_POOLS = Object.freeze({
+//   [NETWORK.mainnet]: {
+//     config: { chainId: NETWORK.mainnet },
+//     prizePool: { address: '0xde9ec95d7708B8319CCca4b8BC92c0a3B70bf416' },
+//     prizeStrategy: { address: '0x3D9946190907aDa8b70381b25c71eB9adf5f9B7b' },
+//     tokens: {
+//       ticket: {
+//         decimals: 6,
+//         address: '0xD81b1A8B1AD00Baa2D6609E0BAE28A38713872f7'
+//       },
+//       underlyingToken: {
+//         decimals: 6,
+//         address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+//       }
+//     }
+//   },
+//   [NETWORK.rinkeby]: {
+//     config: { chainId: NETWORK.rinkeby },
+//     prizePool: { address: '0x4706856fa8bb747d50b4ef8547fe51ab5edc4ac2' },
+//     prizeStrategy: { address: '0x5E0A6d336667EACE5D1b33279B50055604c3E329' },
+//     tokens: {
+//       ticket: {
+//         decimals: 18,
+//         address: '0x4fb19557fbd8d73ac884efbe291626fd5641c778'
+//       },
+//       underlyingToken: {
+//         decimals: 18,
+//         address: '0x5592ec0cfb4dbc12d3ab100b257153436a1f0fea'
+//       }
+//     }
+//   }
+// })
+// const MOCK_PRIZES = {
+//   0: [
+//     {
+//       amountUnformatted: ethers.BigNumber.from('5000000000000000000'),
+//       distributionIndex: 2,
+//       drawId: 0,
+//       pickIndex: 4
+//     }
+//   ],
+//   1: [
+//     {
+//       amountUnformatted: ethers.BigNumber.from('5000000000000000000'),
+//       distributionIndex: 2,
+//       drawId: 1,
+//       pickIndex: 4
+//     }
+//   ],
+//   2: [
+//     {
+//       amountUnformatted: ethers.BigNumber.from('5000000000000000000'),
+//       distributionIndex: 2,
+//       drawId: 2,
+//       pickIndex: 13
+//     },
+//     {
+//       amountUnformatted: ethers.BigNumber.from('10000000000000000000'),
+//       distributionIndex: 1,
+//       drawId: 2,
+//       pickIndex: 1
+//     }
+//   ],
+//   3: [
+//     {
+//       amountUnformatted: ethers.BigNumber.from('10000000000000000000000'),
+//       distributionIndex: 0,
+//       drawId: 3,
+//       pickIndex: 1
+//     }
+//   ]
+// }
 
-export const getUsersPrizes = async (_chainId, prizePoolAddress, usersAddress, _draws) => {
-  const chainId = Number(_chainId)
-  const draws = _draws.map(Number)
+// export const getUsersPrizes = async (_chainId, prizePoolAddress, usersAddress, _draws) => {
+//   const chainId = Number(_chainId)
+//   const draws = _draws.map(Number)
 
-  if (!SUPPORTED_NETWORKS.includes(chainId)) {
-    throw new Error('Invalid chain id')
-  } else if (!isValidAddress(usersAddress)) {
-    throw new Error('Invalid user address')
-  } else if (!isValidAddress(prizePoolAddress) || !MOCK_POOLS[chainId]) {
-    throw new Error('Invalid prize pool address')
-  }
+//   if (!SUPPORTED_NETWORKS.includes(chainId)) {
+//     throw new Error('Invalid chain id')
+//   } else if (!isValidAddress(usersAddress)) {
+//     throw new Error('Invalid user address')
+//   } else if (!isValidAddress(prizePoolAddress) || !MOCK_POOLS[chainId]) {
+//     throw new Error('Invalid prize pool address')
+//   }
 
-  // TODO: Create Tsunami instance
+//   // TODO: Create Tsunami instance
 
-  // TODO: Fetch latest draw # - It'd be nice to get this into some script that listens to events and updates the KV accordingly.
-  const currentDrawId = 4
+//   // TODO: Fetch latest draw # - It'd be nice to get this into some script that listens to events and updates the KV accordingly.
+//   const currentDrawId = 4
 
-  const isBelowCurrentDrawId = (drawId) => drawId < currentDrawId
-  if (!draws.every(isBelowCurrentDrawId)) {
-    throw new Error('Invalid draw ids')
-  }
+//   const isBelowCurrentDrawId = (drawId) => drawId < currentDrawId
+//   if (!draws.every(isBelowCurrentDrawId)) {
+//     throw new Error('Invalid draw ids')
+//   }
 
-  // Update prizes
-  await updatePrizes(chainId, prizePoolAddress, usersAddress, draws)
+//   // Update prizes
+//   await updatePrizes(chainId, prizePoolAddress, usersAddress, draws)
 
-  // Get requested prizes
-  const allPrizes = getUsersPrizesFromKV(chainId, prizePoolAddress, usersAddress)
-  const prizes = {}
-  draws.forEach((draw) => (prizes[draw] = allPrizes[draw]))
-  return prizes
-}
+//   // Get requested prizes
+//   const allPrizes = getUsersPrizesFromKV(chainId, prizePoolAddress, usersAddress)
+//   const prizes = {}
+//   draws.forEach((draw) => (prizes[draw] = allPrizes[draw]))
+//   return prizes
+// }
 
-const getUsersPrizesFromKV = (chainId, prizePoolAddress, usersAddress) => {
-  let allPrizes = TSUNAMI_PRIZES.get(getUsersPrizesKey(chainId, prizePoolAddress, usersAddress))
-  return allPrizes || {}
-}
+// const getUsersPrizesFromKV = (chainId, prizePoolAddress, usersAddress) => {
+//   let allPrizes = TSUNAMI_PRIZES.get(getUsersPrizesKey(chainId, prizePoolAddress, usersAddress))
+//   return allPrizes || {}
+// }
 
-const getUsersPrizesKey = (chainId, prizePoolAddress, usersAddress) =>
-  `${chainId}-${prizePoolAddress}-${usersAddress}`
+// const getUsersPrizesKey = (chainId, prizePoolAddress, usersAddress) =>
+//   `${chainId}-${prizePoolAddress}-${usersAddress}`
 
-const updatePrizes = async (chainId, prizePoolAddress, usersAddress, draws) => {
-  // TODO: Create Tsunami Player instance
-  const allPrizes = getUsersPrizesFromKV(chainId, prizePoolAddress, usersAddress)
+// const updatePrizes = async (chainId, prizePoolAddress, usersAddress, draws) => {
+//   // TODO: Create Tsunami Player instance
+//   const allPrizes = getUsersPrizesFromKV(chainId, prizePoolAddress, usersAddress)
 
-  // Fetch any missing prizes
-  const promises = []
-  draws.forEach((draw) => {
-    if (!allPrizes[draw]) {
-      // TODO: Fetch the prizes for the specific draw & push to array
-      promises.push(
-        new Promise((resolve) => {
-          resolve({
-            drawId: draw,
-            prizes: MOCK_PRIZES[draw]
-          })
-        })
-      )
-    }
-  })
+//   // Fetch any missing prizes
+//   const promises = []
+//   draws.forEach((draw) => {
+//     if (!allPrizes[draw]) {
+//       // TODO: Fetch the prizes for the specific draw & push to array
+//       promises.push(
+//         new Promise((resolve) => {
+//           resolve({
+//             drawId: draw,
+//             prizes: MOCK_PRIZES[draw]
+//           })
+//         })
+//       )
+//     }
+//   })
 
-  const results = await Promise.allSettled(promises)
+//   const results = await Promise.allSettled(promises)
 
-  results.forEach((result) => {
-    const { status, value } = result
-    if (status === 'rejected') {
-      throw new Error('Error fetching users prizes')
-    }
-    allPrizes[value.drawId] = value.prizes
-  })
+//   results.forEach((result) => {
+//     const { status, value } = result
+//     if (status === 'rejected') {
+//       throw new Error('Error fetching users prizes')
+//     }
+//     allPrizes[value.drawId] = value.prizes
+//   })
 
-  // Update KV if we fetched anything
-  if (results.length) {
-    TSUNAMI_PRIZES.set(getUsersPrizesKey(chainId, prizePoolAddress, usersAddress), allPrizes)
-  }
-}
+//   // Update KV if we fetched anything
+//   if (results.length) {
+//     TSUNAMI_PRIZES.set(getUsersPrizesKey(chainId, prizePoolAddress, usersAddress), allPrizes)
+//   }
+// }
--- packages/api-tsunami-prizes/index.js
+++ packages/api-tsunami-prizes/index.js
@@ -1,6 +1,10 @@
 import { log } from '../../utils/sentry'
 import { DEFAULT_HEADERS } from '../../utils/constants'
-import { getUsersPrizes } from './getUsersPrizes'
+// import { getUsersPrizes } from './getUsersPrizes'
+import { Contract, ethers } from 'ethers'
+import { ERC20Abi } from '../../abis/ERC20Abi'
+import { contract } from '@pooltogether/etherplex'
+import { batch } from './cloudflare-workers-batch'
 
 addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event))
@@ -12,56 +16,74 @@ addEventListener('fetch', (event) => {
  * @param {Event} event
  */
 async function handleRequest(event) {
-  setInfuraId(INFURA_ID)
-  setQuicknodeId(QUICKNODE_ID)
-
   try {
     const request = event.request
     const _url = new URL(request.url)
     const pathname = _url.pathname
 
-    const splitPathname = pathname.split('/')
-    const chainId = splitPathname[0]
-    const prizePoolAddress = splitPathname[1]
-    const usersAddress = splitPathname[3]
-    const draws = _url.searchParams.get('draws')
-
-    // Routes
-    if (true) {
-      const prizes = await getUsersPrizes(chainId, prizePoolAddress, usersAddress, draws)
-      const jsonBody = JSON.stringify(prizes, null, 2)
-      const successResponse = new Response(jsonBody, {
-        ...DEFAULT_HEADERS,
-        status: 200
-      })
-      return successResponse
+    // TESTING
+    const tokenAddress = '0x26c6cc6422fefe460f48ea4971997fb97c33abac'
+    const tokenContract = contract(tokenAddress, ERC20Abi, tokenAddress)
+    let batchCalls = []
+    batchCalls.push(
+      tokenContract
+        .decimals()
+        .name()
+        .symbol()
+        .totalSupply()
+    )
+    const response = await batch(4, ...batchCalls)
+    const data = {
+      decimals: response[tokenAddress].decimals[0],
+      name: response[tokenAddress].name[0],
+      symbol: response[tokenAddress].symbol[0],
+      totalSupplyUnformatted: response[tokenAddress].totalSupply[0],
+      totalSupply: ethers.utils.formatUnits(
+        response[tokenAddress].totalSupply[0],
+        response[tokenAddress].decimals[0]
+      )
     }
-
-    // Invalid path
-    const notFoundResponse = new Response('Invalid request', {
+    const jsonBody = JSON.stringify(data, null, 2)
+    const successResponse = new Response(jsonBody, {
       ...DEFAULT_HEADERS,
-      status: 404
+      status: 200
     })
-    notFoundResponse.headers.set('Content-Type', 'text/plain')
+    return successResponse
 
-    return notFoundResponse
+    // const splitPathname = pathname.split('/')
+    // const chainId = splitPathname[0]
+    // const prizePoolAddress = splitPathname[1]
+    // const usersAddress = splitPathname[3]
+    // const draws = _url.searchParams.get('draws')
+
+    // // Routes
+    // if (true) {
+    //   const prizes = await getUsersPrizes(chainId, prizePoolAddress, usersAddress, draws)
+    //   const jsonBody = JSON.stringify(prizes, null, 2)
+    //   const successResponse = new Response(jsonBody, {
+    //     ...DEFAULT_HEADERS,
+    //     status: 200
+    //   })
+    //   return successResponse
+    // }
+
+    // // Invalid path
+    // const notFoundResponse = new Response('Invalid request', {
+    //   ...DEFAULT_HEADERS,
+    //   status: 404
+    // })
+    // notFoundResponse.headers.set('Content-Type', 'text/plain')
+
+    // return notFoundResponse
   } catch (e) {
     // Error
     event.waitUntil(log(e, e.request))
 
-    const errorResponse = new Response(e?.message || 'Error', {
+    const errorResponse = new Response('Error', {
       ...DEFAULT_HEADERS,
       status: 500
     })
     errorResponse.headers.set('Content-Type', 'text/plain')
     return errorResponse
   }
-}
-
-function setInfuraId(INFURA_ID) {
-  throw new Error('Function not implemented.')
-}
-
-function setQuicknodeId(QUICKNODE_ID) {
-  throw new Error('Function not implemented.')
 }
