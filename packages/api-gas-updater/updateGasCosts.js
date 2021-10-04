8ccfb795dc092417d6b19f810f79c68edfb3d698
Charles Bergeron <chuck.bergeron@gmail.com>
2021-10-04 11:40:49 -0700

    * fixes/cleanup

--- packages/api-gas-updater/updateGasCosts.js
+++ packages/api-gas-updater/updateGasCosts.js
@@ -1,8 +1,13 @@
+import cheerio from 'cheerio'
+
 import { log } from '../../utils/sentry'
 import { getGasKey } from '../../utils/kvKeys'
 
 // Confirmation time!
 // `https://api.etherscan.io/api?module=gastracker&action=gasestimate&gasprice=2000000000&apiKey=${ETHERSCAN_API_KEY}`
+
+const MAINNET_CHAIN_ID = 137
+const POLYGON_CHAIN_ID = 137
 
 /**
  * Get latest gas cost for chain and store the response in cloudflares KV
@@ -14,7 +19,7 @@ export const updateGasCosts = async (event, chainId) => {
   let gasCosts
 
   // Uses Etherscan's API
-  if (chainId === 1) {
+  if (chainId === MAINNET_CHAIN_ID) {
     const response = await fetch(
       `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apiKey=${ETHERSCAN_API_KEY}`
     )
@@ -22,7 +27,7 @@ export const updateGasCosts = async (event, chainId) => {
   }
 
   // Scrapes PolygonScan's gastracker html page (no API)
-  if (chainId === 56) {
+  if (chainId === POLYGON_CHAIN_ID) {
     gasCosts = await scrapePolygonScan()
   }
 
@@ -40,37 +45,27 @@ export const updateGasCosts = async (event, chainId) => {
 }
 
 const scrapePolygonScan = async () => {
-  const fetch = require('node-fetch')
-  const cheerio = require('cheerio')
+  const response = await fetch(`https://polygonscan.com/gastracker`)
+  const body = await response.text()
 
-  let gasCosts
+  const $ = cheerio.load(body)
+  const standard = $('span#standardgas')
+    .text()
+    .split(' ')[0]
+  const fast = $('span#fastgas')
+    .text()
+    .split(' ')[0]
+  const rapid = $('span#rapidgas')
+    .text()
+    .split(' ')[0]
 
-  const get = async () => {
-    const response = await fetch(`https://polygonscan.com/gastracker`)
-    const body = await response.text()
-
-    const $ = cheerio.load(body)
-    const standard = $('span#standardgas')
-      .text()
-      .split(' ')[0]
-    const fast = $('span#fastgas')
-      .text()
-      .split(' ')[0]
-    const rapid = $('span#rapidgas')
-      .text()
-      .split(' ')[0]
-
-    const result = {
-      result: {
-        SafeGasPrice: standard,
-        ProposeGasPrice: fast,
-        FastGasPrice: rapid
-      }
+  const result = {
+    result: {
+      SafeGasPrice: standard,
+      ProposeGasPrice: fast,
+      FastGasPrice: rapid
     }
-
-    gasCosts = JSON.stringify(result)
-    console.log(gasCosts)
   }
 
-  get()
+  return JSON.stringify(result)
 }
