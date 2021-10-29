ad4c96440188e2f9c4849df59a44cb91bd9078f2
Charles Bergeron <chuck.bergeron@gmail.com>
2021-10-28 17:56:42 -0700

    * got it working

--- packages/api-discord-invite/index.js
+++ packages/api-discord-invite/index.js
@@ -1,86 +1,33 @@
+import { createVerifier } from "@glenstack/cf-workers-hcaptcha";
+
 import { DEFAULT_HEADERS } from '../../utils/constants'
-import { log } from '../../utils/sentry'
 
-const { verify } = require('hcaptcha');
-
-/**
- * @param {Event} event
- */
-async function handleRequest(event) {
+const handleRequest = async (event) => {
   const request = event.request
-  const formData = await request.formData();
-  const body = Object.fromEntries(formData);
-  const token = body.hCaptchaToken
+  const verify = createVerifier(H_CAPTCHA_SECRET);
 
-  verify(secret, token).then(data => console.log(data)).catch(e => console.error(e))
+  try {
+    const payload = await verify(request);
 
-  // H_CAPTCHA_SECRET
-  const hCaptchaResponse = await verify(secret, token)
-  console.log('hCaptchaResponse')
-  console.log(hCaptchaResponse)
-  const data = hCaptchaResponse
-  console.log('data')
-  console.log(data)
-
-  return new Response('ho', {
-    status: 200
-  })
-
-  // const data = hCaptchaResponse.data
-
-  // let successResponse
-  // if (data) {
-  //   let responseBody
-
-  //   console.log(data)
-
-  //   if (data.success === true) {
-  //     console.log('success!', data);
-  //     console.log(data)
-  //     responseBody = 'sccess'
-  //   } else {
-  //     console.log('verification failed');
-  //     responseBody = 'fail'
-  //   }
-
-  //   successResponse = new Response(responseBody, {
-  //     ...DEFAULT_HEADERS,
-  //     status: 200
-  //   })
-  //   successResponse.headers.set('Content-Type', 'text/plain')
-    
-  // }
-
-  // return successResponse
-  
-    // })
-    // .catch((e) => {
-    //   event.waitUntil(log(e, e.request))
-
-    //   console.log('err')
-
-    //   const errorResponse = new Response(`Error`, {
-    //     ...DEFAULT_HEADERS,
-    //     status: 500
-    //   })
-    //   errorResponse.headers.set('Content-Type', 'text/plain')
-
-    //   return errorResponse
-    // });
-}
+    if (payload.success) {
+      const discordInviteToken = 'kalsjd'
+      
+      return new Response(discordInviteToken, {
+        ...DEFAULT_HEADERS,
+        status: 200
+      })
+    } else {
+      throw new Error('not verified')
+    }
+  } catch (e) {
+    console.log(e.message)
+    return new Response(`Could not verify hCaptcha token: ${e.message}`, {
+      ...DEFAULT_HEADERS,
+      status: 401,
+    });
+  }
+};
 
 addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event))
 })
-
-// export default {
-//   async fetch(request) {
-//     console.log(request)
-    
-//     if (request.method === "POST") {
-//       return handleRequest(request);
-//     }
-
-//     return new Response("Method Not Allowed", { status: 405 });
-//   },
-// };
